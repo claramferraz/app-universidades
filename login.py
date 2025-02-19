6f43b695b6d2bc262dbfc149c72aea608b04d59a
Maria Clara Ferraz <167479746+claramferraz@users.noreply.github.com>
2025-02-19 16:05:10 -0300

    Update login.py

--- login.py
+++ login.py
@@ -36,6 +36,55 @@ def visualizar_postagens():
             print("Professor ou título inválidos")
             return
         
+enquetes = {}
+
+def criar_enquete():
+    professor = input("Digite seu nome:")
+    enquete = {}
+    enquetes[professor] = enquete
+    num_perguntas = int(input("Digite o número de perguntas da enquete: "))
+
+    for i in range(num_perguntas):
+        pergunta = input(f"Digite o enunciado da pergunta {i + 1}: ")
+        opcoes = []
+        while True:
+            opcao = input("Digite uma opção para a pergunta (ou 'fim' para terminar): ")
+            if opcao.lower() == "fim":
+                break
+            opcoes.append(opcao)
+        enquete[pergunta] = opcoes
+
+    print("\nEnquete criada:")
+    for pergunta, opcoes in enquete.items():
+        print(f"\n{pergunta}")
+        for i, opcao in enumerate(opcoes):
+            print(f"{i + 1}. {opcao}")
+
+    return enquete
+
+respostas_alunos = {}
+
+def visualizar_enquete():
+    print(enquetes)
+    enquete = input("Digite o nome do professor para acessar enquete:")
+
+    if enquete not in enquetes:
+        print("Esse professor não criou uma enquete")
+    else:
+        print(enquetes[enquete])
+        respostas=[]
+        resposta = input("Digite sua resposta para a enquete(ex: 1, 2):")
+        respostas.append(resposta)
+        respostas_alunos[enquete] = respostas
+
+def respostas_enquete():
+    enquete =input("Digite seu nome:")
+    if enquete in respostas_alunos:
+        print(respostas_alunos[enquete])
+    else:
+        print("Não há enquetes cadastradas para esse nome")
+
+
 def login():
     user = input("Digite seu nome igual no cadastro:").strip()
 
@@ -56,9 +105,9 @@ def login():
                 if menu == 1:
                     criar_postagem()
                 elif menu == 2:
-                    print("CRIAR ENQUETE")
+                    criar_enquete()
                 else:
-                    print("VER ENQUETE")
+                    respostas_enquete()
         else:
             print("Menu:")
             print("(1) Visualizar postagens")
@@ -70,7 +119,7 @@ def login():
                 if menu == 1:
                     visualizar_postagens()
                 else:
-                    print("VISUALIZAR ENQUETE")
+                    visualizar_enquete()
 while True:
     login()
 
